7304e9ff11c34e3e3b62527d0c8b4576e84ad970
JJShen2000 <gimgim7910@gmail.com>
2022-04-28 20:52:37 +0800

    Add a testcase of cdr file (3 cdr)

--- cdrFile/cdrfile_test.go
+++ cdrFile/cdrfile_test.go
@@ -1,10 +1,11 @@
 package util
 
 import (
-	// "fmt"
+	"fmt"
 	// "reflect"
 	"testing"
 	"strconv"
+	"os"
 
 	"github.com/stretchr/testify/require"
 )
@@ -12,31 +13,28 @@ import (
 func TestCdrFile(t *testing.T) {
 	t.Parallel()
 
-	// timeNow := time.Now()
-	cdrf := CdrFileHeader{
-		FileLength:                            71,
-		HeaderLength:                          63,
-		HighReleaseIdentifier:                 2,
-		HighVersionIdentifier:                 3,
-		LowReleaseIdentifier:                  4,
-		LowVersionIdentifier:                  5,
-		FileOpeningTimestamp:                  CdrHdrTimeStamp{4, 28, 17, 18, 1, 8, 0},
-		TimestampWhenLastCdrWasAppendedToFIle: CdrHdrTimeStamp{1, 2, 3, 4, 1, 6, 30},
-		NumberOfCdrsInFile:                    1,
-		FileSequenceNumber:                    11,
-		FileClosureTriggerReason:              4,
-		IpAddressOfNodeThatGeneratedFile:      [20]byte{0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb},
-		LostCdrIndicator:          4,
-		LengthOfCdrRouteingFilter: 4,
-		CDRRouteingFilter:                     []byte("abcd"),
-		LengthOfPrivateExtension: 5,
-		PrivateExtension:                      []byte("fghjk"), // vendor specific
-		HighReleaseIdentifierExtension: 2,
-		LowReleaseIdentifierExtension:  3,
-	}
-
 	cdrFile1 := CDRFile{
-		hdr: cdrf,
+		hdr: CdrFileHeader{
+			FileLength:                            71,
+			HeaderLength:                          63,
+			HighReleaseIdentifier:                 2,
+			HighVersionIdentifier:                 3,
+			LowReleaseIdentifier:                  4,
+			LowVersionIdentifier:                  5,
+			FileOpeningTimestamp:                  CdrHdrTimeStamp{4, 28, 17, 18, 1, 8, 0},
+			TimestampWhenLastCdrWasAppendedToFIle: CdrHdrTimeStamp{1, 2, 3, 4, 1, 6, 30},
+			NumberOfCdrsInFile:                    1,
+			FileSequenceNumber:                    11,
+			FileClosureTriggerReason:              4,
+			IpAddressOfNodeThatGeneratedFile:      [20]byte{0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb, 0xa, 0xb},
+			LostCdrIndicator:          4,
+			LengthOfCdrRouteingFilter: 4,
+			CDRRouteingFilter:                     []byte("abcd"),
+			LengthOfPrivateExtension: 5,
+			PrivateExtension:                      []byte("fghjk"), // vendor specific
+			HighReleaseIdentifierExtension: 2,
+			LowReleaseIdentifierExtension:  3,
+		},
 		cdrList: []CDR{{
 			hdr:CdrHeader {
 				CdrLength                  :3,
@@ -50,11 +48,71 @@ func TestCdrFile(t *testing.T) {
 		},},
 	}
 
+	cdrFile2 := CDRFile{
+		hdr: CdrFileHeader{
+			FileLength:                            92,
+			HeaderLength:                          66,
+			HighReleaseIdentifier:                 4,
+			HighVersionIdentifier:                 5,
+			LowReleaseIdentifier:                  5,
+			LowVersionIdentifier:                  6,
+			FileOpeningTimestamp:                  CdrHdrTimeStamp{1, 2, 11, 56, 1, 7, 30},
+			TimestampWhenLastCdrWasAppendedToFIle: CdrHdrTimeStamp{4, 3, 2, 1, 0, 4, 0},
+			NumberOfCdrsInFile:                    3,
+			FileSequenceNumber:                    65,
+			FileClosureTriggerReason:              2,
+			IpAddressOfNodeThatGeneratedFile:      [20]byte{0xc, 0xd, 0xc, 0xd, 0xc, 0xd, 0xc, 0xd, 0xc, 0xd, 0xc, 0xd, 0xc, 0xd, 0xc, 0xd, 0xc, 0xd, 0xc, 0xd},
+			LostCdrIndicator:          			   4,
+			LengthOfCdrRouteingFilter: 			   5,
+			CDRRouteingFilter:                     []byte("gfdss"),
+			LengthOfPrivateExtension:    	       7,
+			PrivateExtension:                      []byte("abcdefg"), // vendor specific
+			HighReleaseIdentifierExtension: 	   1,
+			LowReleaseIdentifierExtension:  	   2,
+		},
+		cdrList: []CDR{
+			{
+				hdr:CdrHeader {
+					CdrLength                  :3,
+					ReleaseIdentifier          :Rel6, 
+					VersionIdentifier          :3,            
+					DataRecordFormat           :UnalignedPackedEncodingRules,  
+					TsNumber                   : TS32253,   
+					ReleaseIdentifierExtension :4,
+				},
+				cdrByte:[]byte("abc"),
+			},
+			{
+				hdr:CdrHeader {
+					CdrLength                  :6,
+					ReleaseIdentifier          :Rel5,
+					VersionIdentifier          :2,               
+					DataRecordFormat           :AlignedPackedEncodingRules1,  
+					TsNumber                   : TS32205,   
+					ReleaseIdentifierExtension :2,
+				},
+				cdrByte:[]byte("ghjklm"),
+			},
+			{
+				hdr:CdrHeader {
+					CdrLength                  :2,
+					ReleaseIdentifier          :Rel9,
+					VersionIdentifier          :3,               
+					DataRecordFormat           :AlignedPackedEncodingRules1,  
+					TsNumber                   : TS32225,  
+					ReleaseIdentifierExtension :1,
+				},
+				cdrByte:[]byte("cv"),
+			},
+		},
+	}
+
 	testCases := []struct {
 		name  string
 		in    CDRFile
 	}{
 		{"cdrfile1", cdrFile1},
+		{"cdrfile2", cdrFile2},
 	}
 
 	for i, tc := range testCases {
@@ -63,6 +121,10 @@ func TestCdrFile(t *testing.T) {
 			tc.in.Encoding(fileName)
 			newCdrFile := CDRFile{}
 			newCdrFile.Decoding(fileName)
+			e := os.Remove(fileName)
+			if e != nil {
+				fmt.Println(e)
+			}
 
 			require.Equal(t, tc.in, newCdrFile)
 			// require.True(t, reflect.DeepEqual(tc.in, newCdrFile))
